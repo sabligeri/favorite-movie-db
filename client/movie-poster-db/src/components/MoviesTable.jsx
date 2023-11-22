9d24300a5cd87c6cd58782052b74523b79e14848
Gergely Sabli <139555565+sabligeri@users.noreply.github.com>
2023-11-22 11:02:34 +0100

    making add to favorites button and its function

--- client/movie-poster-db/src/components/MoviesTable.jsx
+++ client/movie-poster-db/src/components/MoviesTable.jsx
@@ -1,4 +1,24 @@
 const MoviesTable = ({ movies }) => {
+
+  function handleSaveToFavMovies(movies) {
+    const poster = movies.Poster;
+    const title = movies.Title;
+    const year = movies.Year;
+    fetch('/api/favmovies', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ poster, title, year })
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   return (
     <table>
         <tbody>
@@ -6,8 +26,9 @@ const MoviesTable = ({ movies }) => {
             {movies.map(movie => (
               <th key={movie.imdbID}>
                 <img src={(movie.Poster !== 'N/A') ? movie.Poster : './src/noMoviePoster.jpg'} />
-                <h3>Title: {movie.Title}</h3>
+                <h3>Title: {movie.Title} </h3>
                 <h4>Released Date: {movie.Year}</h4>
+                <button onClick={() => handleSaveToFavMovies(movies)}>Add to favorites</button>
               </th>
             )
             )}
